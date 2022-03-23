5d141d1f5254df3b8b46ba80126e353ef755bc56
Zach <zawood11@gmail.com>
2022-03-23 02:10:09 -0400

    fix stocks controller

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -3,22 +3,22 @@ class StocksController < ApplicationController
     
     #GET: /stocks
     get "/stocks" do
-        Stocks.all.to_json
+        Stock.all.to_json
     end
 
     #GET: /stocks/id
     get "/stocks/:id" do
-        Stocks.find(params[:id]).to_json
+        Stock.find(params[:id]).to_json
     end
 
     #POST: /stocks
     post "/stocks" do
-        Stocks.create(params).to_json
+        Stock.create(params).to_json
     end
 
     #DELETE: /stocks/id
     delete "/stocks/id" do
-        Stocks.find(params[:id]).destroy.to_json
+        Stock.find(params[:id]).destroy.to_json
     end
   
 end
