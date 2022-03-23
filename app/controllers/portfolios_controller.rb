cb74c78ff014158d26b7f94c71d3dc96238a7a7e
Zach <zawood11@gmail.com>
2022-03-23 00:11:29 -0400

    build out portfolios controller

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -5,5 +5,28 @@ class ApplicationController < Sinatra::Base
     get "/portfolios" do
       Portfolio.all.to_json
     end
-  
+
+    # GET: /portfolios/id
+    get "/portfolios/:id" do
+        Portfolio.find(params[:id]).to_json
+    end
+
+    #POST: /portfolios
+    post "/portfolios" do
+        Portfolio.create(params)
+    end
+
+    #PATCH: /portfolios/id
+    patch "/portfolios/:id" do 
+        Portfolio.find(params[:id]).update(
+            name: params[:name]
+        )
+    end
+
+    #DELETE: /portfolios/id
+    delete "/portfolios/:id" do
+        Portfolio.find(params[:id]).destroy
+    end
+    
+
 end
