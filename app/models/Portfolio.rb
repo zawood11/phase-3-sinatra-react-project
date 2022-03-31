1829a56ca150dc656198ce00fd451a1cf294852a
Zach <zawood11@gmail.com>
2022-03-31 14:33:34 -0400

    add portfolio name uniqueness validation

--- app/models/Portfolio.rb
+++ app/models/Portfolio.rb
@@ -2,4 +2,6 @@ class Portfolio < ActiveRecord::Base
     has_many :positions
     has_many :stocks, through: :positions
     has_many :prices, through: :stocks
+
+    validates :name, uniqueness: true
 end
